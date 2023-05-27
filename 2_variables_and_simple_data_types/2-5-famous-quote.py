67b5fde052ecf46258466f7924bf5e411496ada9
Olivier_Ribon <oli.ribon@gmail.com>
2023-05-27 08:51:42 +0200

    <traite les exos 5 et 6 : citations

---
+++ 2_variables_and_simple_data_types/2-5-famous-quote.py
@@ -0,0 +1,2 @@
+message = 'Le défunt Daniel Johnson,le premier ministre du Québec dans les années 1960, se plaisait à dire "Quand je me regarde, je me désole; quand je me compare, je me console"'
+print(message) 
---
+++ 2_variables_and_simple_data_types/2-6-famous-quote-2.py
@@ -0,0 +1,3 @@
+nom_personnage_fameux = 'Le défunt Daniel Johnson,le premier ministre du Québec dans les années 1960,'
+message = f'{nom_personnage_fameux} se plaisait à dire "Quand je me regarde, je me désole; quand je me compare, je me console"'
+print(message) 
