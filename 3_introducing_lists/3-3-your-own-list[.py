3136f7df41945ddb842fa72976961c4e72338186
Olivier_Ribon <oli.ribon@gmail.com>
2023-05-28 11:54:11 +0200

    Traite l'exo 3.3 : intro sur les listes

---
+++ 3_introducing_lists/3-3-your-own-list[.py
@@ -0,0 +1,8 @@
+infos_sur_mes_autos = [
+"Ma première auto était une 104",
+"La meilleure auto que j'ai conduite était une opel vectra turbo diesel",
+"Ma prochaine auto aura une motorisation électrique"
+]
+print(infos_sur_mes_autos[0])
+print(infos_sur_mes_autos[1])
+print(infos_sur_mes_autos[2])
