d9d84f920e7edb9bf3276133f5deabc78889b54e
Olivier_Ribon <oli.ribon@gmail.com>
2023-06-05 21:41:54 +0200

    Traite les exos 3-4 et 3-( : listes sur les invités

---
+++ 3_introducing_lists/3-5-changing-guest-lists.py
@@ -0,0 +1,13 @@
+guest_list=["Papa","Véronique","Manon","Arnaud","Gaëlle"]
+print(f"Bonjour {guest_list[0]}, je t'invite à mon annivesaire le 30 mai à 21H00")
+print(f"Bonjour {guest_list[1]}, je t'invite à mon annivesaire le 30 mai à 21H00")
+print(f"Bonjour {guest_list[2]}, je t'invite à mon annivesaire le 30 mai à 21H00")
+print(f"Bonjour {guest_list[3]}, je t'invite à mon annivesaire le 30 mai à 21H00")
+print(f"Bonjour {guest_list[4]}, je t'invite à mon annivesaire le 30 mai à 21H00")
+print(f"{guest_list[2]} ne pourra pas venir. J'invite Nathalie")
+guest_list[2] = "Nathalie"
+print(f"Bonjour {guest_list[0]}, je t'invite à mon annivesaire le 30 mai à 21H00")
+print(f"Bonjour {guest_list[1]}, je t'invite à mon annivesaire le 30 mai à 21H00")
+print(f"Bonjour {guest_list[2]}, je t'invite à mon annivesaire le 30 mai à 21H00")
+print(f"Bonjour {guest_list[3]}, je t'invite à mon annivesaire le 30 mai à 21H00")
+print(f"Bonjour {guest_list[4]}, je t'invite à mon annivesaire le 30 mai à 21H00")
