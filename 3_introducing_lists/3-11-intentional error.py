724772a4b0dcc77e7cb4c06d538c6657c67a2369
Olivier_Ribon <oli.ribon@gmail.com>
2023-06-07 22:12:19 +0200

    Traite l'exo 3.11 : exemple d'index error

---
+++ 3_introducing_lists/3-11-intentional error.py
@@ -0,0 +1,14 @@
+mes_villes = ["Avignon","Clermont-Ferrand","Aix-en-Provence"]
+
+# erreur : l'index 3 de cette liste n'existe pas
+#print(mes_villes[3])
+#Traceback (most recent call last):
+# File "c:\Users\oliri\OneDrive\Documents\python\python_crash_course\3_introducing_lists\3-11-intentional error.py", line 2, in <module>
+#    print(mes_villes[3])
+#          ~~~~~~~~~~^^^
+#IndexError: list index out of range
+
+#Cette fois ci c'est OK
+
+print(mes_villes[2])
+
