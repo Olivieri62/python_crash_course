d08a9fbb7fbd1a23df370fe88c35bd574c2c06a8
Olivier_Ribon <oli.ribon@gmail.com>
2023-06-10 17:39:12 +0200

    Traite les exos 4.1 et 4.2 : itérer sur les listes

---
+++ 4_working_with_lists/4-1-pizzas.py
@@ -0,0 +1,9 @@
+pizzas = ["4 fromages","Pescatore","4 saisons"]
+
+for pizza in pizzas :
+    print(pizza)
+
+for pizza in pizzas:
+    print(f"J'aime la pizza {pizza}")
+
+print("J'adore les pizzas")
---
+++ 4_working_with_lists/4-2-animals.py
@@ -0,0 +1,9 @@
+animals = ["chien","chat","hamster"]
+
+for animal in animals:
+    print(animal)
+
+for animal in animals :
+    print(f"Un {animal} ferait un excellent animal de compagnie.")
+
+print("Tous ces animaux feraient d'excellents animaux de compagnie!")
