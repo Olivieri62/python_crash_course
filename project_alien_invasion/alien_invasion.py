ee8b33090d345a54ceb664dba322b3b927a4b588
Olivier_Ribon <oli.ribon@gmail.com>
2023-08-01 11:38:20 +0200

    Modifie la calsse AlienInvasion : contrôle de la férquence d'images et paramétrer  la couleur de l'arrière plan

--- project_alien_invasion/alien_invasion.py
+++ project_alien_invasion/alien_invasion.py
@@ -8,9 +8,13 @@ class AlienInvasion:
     def __init__(self):
         """Initialize te game, and create game ressources"""
         pygame.init()
-
+        #Create an instance of the class Clock from the pygame.time module
+        self.clock = pygame.time.Clock()
         self.screen = pygame.display.set_mode((1200,800))
         pygame.display.set_caption("Alien Invasion")
+
+        # Set the Background color
+        self.bg_color=(230,230,230)
 
     def run_game(self):
         """Start the main loop for the game"""
@@ -19,8 +23,12 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+            #Redraw the screen during each pass trhrough the loop
+            self.screen.fill(self.bg_color)
             #Make the most recently drawn screen visible
             pygame.display.flip()
+            #Pygame will do its best to make the loop run exactly 60 times per second
+            self.clock.tick(60)
 
 if __name__ == '__main__':
     #Make agame instance, and run the game
