e2ee74cedc6ad6c8cb48aa4a04f38072611fc252
Olivier_Ribon <oli.ribon@gmail.com>
2023-08-07 14:42:58 +0200

    Shoot bullets

--- project_alien_invasion/alien_invasion.py
+++ project_alien_invasion/alien_invasion.py
@@ -4,6 +4,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior"""
@@ -19,6 +20,7 @@ class AlienInvasion:
         self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
         self.ship = Ship(self)
+        self.bullets=pygame.sprite.Group()
 
 
 
@@ -27,6 +29,7 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
+            self.bullets.update()
             #Redraw the screen during each pass trhrough the loop
             self._update_screen()
             #Pygame will do its best to make the loop run exactly 60 times per second
@@ -50,6 +53,8 @@ class AlienInvasion:
             self.ship.moving_left = True
         elif event.key == pygame.K_q:
             sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
 
     def _check_keyup_events(self,event):
         """Respond to keypresses"""
@@ -58,12 +63,21 @@ class AlienInvasion:
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
 
+    def _fire_bullet(self):
+        """Create a new bullet and add it to the bullets group"""
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
+
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen"""
         self.screen.fill(self.settings.bg_color)
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
         self.ship.blitme()
         #Make the most recently drawn screen visible
         pygame.display.flip()
+
+        
 
 if __name__ == '__main__':
     #Make agame instance, and run the game
